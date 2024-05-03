6e4578d2d536cbbdd1e81f04c834e6895e1dd11e
nonso01 <chukwunonsomartin01@gmail.com>
2024-05-03 14:46:49 +0100

    fix

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
 import eruda from "eruda";
 import On from "on-dom";
+import "./style.css";
 
 eruda.init();
 
 const log = console.log;
 
 const dragTarget = document.querySelector("[draggable=true]");
-const dropTargets = Array.from(document.querySelectorAll(".drop-targets"));
+const dropTargets = document.querySelectorAll(".drop-targets");
 
 const feedBackImage = new Image();
+const dataType = "text/plain";
 
-new On(dragTarget, {
+const dragTargetEvents = new On(dragTarget, {
   dragstart(e) {
+    const dT = e.dataTransfer;
+    dT.setData(dataType, "this may be dragged");
+    dT.effectAllowed = "copyMove";
     log(e);
   },
+});
+
+const dropTargetsEvent = new On(dropTargets, {
+  dragover(e) {
+    log(e);
+  },
+  dragenter() {},
+  dragleave() {},
 });
 // const
